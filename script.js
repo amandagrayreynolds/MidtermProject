7216b1f0d2887cc59eec036f8dfff012c690fcfc
Amanda Reynolds <amandagfreynolds@gmail.com>
2016-11-11 16:29:29 -0500

    Amanda's Edit

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+//DIVS AND CLASSES _ 
+$(document).ready(function() {
+var seats = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", 
+"B5","C1","C2","C3","C4","C5","C6","C7","D1","D2","D3","D4",
+"D5","D6","D7","D8"];
 
+ for(var i =0; i< seats.length; i++){
+ var element = seats[i];
+  if (element.charAt(0)==="A"){ 
+    $("#row1").append('<div class="Assie seat" id="'+element+'"">' + seats[i]+ '</div>');
+  } 
+  
+  else if (element.charAt(0)==="B"){
+  $("#row2").append('<div class="Bassie seat">' + seats[i]+ '</div>');  
+  }
+
+  else if (element.charAt(0)==="C"){
+  $("#row3").append('<div class="Cassie seat">' + seats[i]+ '</div>');  
+  }
+
+  else {
+  $("#row4").append('<div class="Dassie seat">' + seats[i]+ '</div>');  
+  }
+
+
+};
+
+//END CLASS
 $(document).ready(function(){
 //seat highlighting//
     $(".seat").mouseenter(function(){
